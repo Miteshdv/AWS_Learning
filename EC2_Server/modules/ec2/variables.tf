d15b1db7a70e49d8a5d620add9e47407a2e45f05
Mitesh Dave <Mitesh.Dave@mphasis.com>
2024-11-30 14:45:59 -0600

    Adding Ec2 server

---
+++ EC2_Server/modules/ec2/variables.tf
@@ -0,0 +1,35 @@
+variable "ami" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key name for the EC2 instance"
+  type        = string
+}
+
+
+variable "iam_instance_profile" {
+  description = "The IAM instance profile for the EC2 instance"
+  type        = string
+}
+
+variable "user_data" {
+  description = "The user data script for the EC2 instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the EC2 instance will be launched"
+  type        = string
+}
